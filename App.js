80beed4b8effc36c0ea9d1e66adce305d4acc8ce
fonze816 <muhammedraafat10@gmail.com>
2021-06-07 14:57:10 +0200

    some other changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>i made some changes</Text>
+      <Text>i made some changessss</Text>
       <StatusBar style="auto" />
     </View>
   );
