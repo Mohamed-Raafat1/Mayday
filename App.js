77c28d611325d9dc9829c820569f9131129951c0
fonze816 <muhammedraafat10@gmail.com>
2021-06-08 11:24:50 +0200

    check point 1  adding some basic nativebase components

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { Container, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default function App() {
+  const [loaded] = useFonts({
+    Roboto: require('native-base/Fonts/Roboto.ttf'),
+    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+  });
   return (
-    <View style={styles.container}>
-      <Text>i made some changessss</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+        <Text>Open up App.js to start working on your app!</Text>
+      </Container>
   );
 }
 
