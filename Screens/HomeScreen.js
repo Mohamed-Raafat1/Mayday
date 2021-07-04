041cbb5ccea9b30fd9558134ab975040209642b8
fonze816 <muhammedraafat10@gmail.com>
2021-07-04 02:26:37 +0200

    removed unecessary button in homescreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,29 +1,28 @@
-import React from 'react';
+import React from "react";
 import { StyleSheet, Button, View, Text, Image } from "react-native";
 
-
 function HomeScreen({ navigation }) {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.sosButton}
-        source={require('../assets/red-button.png')}/>
-        <Button title={"banana"} onPress={() => navigation.navigate("Login")}>
-      </Button>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.sosButton}
+        source={require("../assets/red-button.png")}
+      />
+    </View>
+  );
+}
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    sosButton:{
-        width: 350,
-        height: 350,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sosButton: {
+    width: 350,
+    height: 350,
+  },
 });
