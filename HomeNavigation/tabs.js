1dacaa8fa3dcf9f12b081c92b8a3392c3d22ef98
fonze816 <muhammedraafat10@gmail.com>
2021-07-04 02:25:54 +0200

    removed unecessary button in homescreen

--- HomeNavigation/tabs.js
+++ HomeNavigation/tabs.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 
 import { StyleSheet, Button, View, Text, Image } from "react-native";
-import { createStackNavigator } from '@react-navigation/stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-
-import HomeScreen from '../Screens/HomeScreen'; 
-import FirstAidScreen from '../Screens/FirstAidScreen';
-import DoctorsScreen from '../Screens/DoctorsScreen';
-import MoreScreen from '../Screens/MoreScreen';
-import ProfileScreen from '../Screens/ProfileScreen';
-import editProfileScreen from '../Screens/editProfileScreen';
-import FirstAidSection from '../Screens/FirstAidSection';
-
+import HomeScreen from "../Screens/HomeScreen";
+import FirstAidScreen from "../Screens/FirstAidScreen";
+import DoctorsScreen from "../Screens/DoctorsScreen";
+import MoreScreen from "../Screens/MoreScreen";
+import ProfileScreen from "../Screens/ProfileScreen";
+import editProfileScreen from "../Screens/editProfileScreen";
+import FirstAidSection from "../Screens/FirstAidSection";
 
 const HomeStack = createStackNavigator();
 const FirstAidStack = createStackNavigator();
@@ -22,112 +20,135 @@ const MoreStack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
 function Tabs() {
-    return (
-        <Tab.Navigator
-          initialRouteName="Home"
-          activeColor="#ff1262"
-          barStyle={{ backgroundColor: 'white' }}
-        >
-          <Tab.Screen
-            name="Home"
-            component={HomeStackScreen}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="First Aid"
-            component={FirstAidSection}
-            options={{
-              tabBarLabel: 'First Aids',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="television-play" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Doctors"
-            component={DoctorsStackScreen}
-            options={{
-              tabBarLabel: 'Doctors',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="doctor" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="More"
-            component={MoreStackScreen}
-            options={{
-              tabBarLabel: 'More',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="menu" color={color} size={26} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      );
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#ff1262"
+      barStyle={{ backgroundColor: "white" }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="First Aid"
+        component={FirstAidSection}
+        options={{
+          tabBarLabel: "First Aids",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="television-play"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Doctors"
+        component={DoctorsStackScreen}
+        options={{
+          tabBarLabel: "Doctors",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="doctor" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="More"
+        component={MoreStackScreen}
+        options={{
+          tabBarLabel: "More",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="menu" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
-const HomeStackScreen = ({navigation}) => (
-    <HomeStack.Navigator>
-          <HomeStack.Screen name="Home" component={HomeScreen}
-            options={{
-            headerLeft:null,
-            title:'Home',
-            headerRight: () => (
-              <View style={styles.iconContainer}>
-                <MaterialCommunityIcons name="account-circle-outline" size={26} 
-                onPress={() => navigation.navigate('Profile')}/>
-                <MaterialCommunityIcons name="bell-outline" size={26} />
-                
-              </View>
-            ),
-          }} />
-          <HomeStack.Screen name="Profile" component={ProfileScreen} options={{
-            title: 'Profile',
-            headerRight: () => (
-              <View style={styles.iconContainer}>
-                <MaterialCommunityIcons name="account-edit-outline" size={30}
-                onPress={() => navigation.navigate('editProfile')} />
-              </View>
-            ),
-           }}
-          />
-          <HomeStack.Screen name="editProfile" component={editProfileScreen} options={{
-            title: 'Edit Profile',
-           }}
-          />
-    </HomeStack.Navigator>
+const HomeStackScreen = ({ navigation }) => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        headerLeft: null,
+        title: "Home",
+        headerRight: () => (
+          <View style={styles.iconContainer}>
+            <MaterialCommunityIcons
+              name="account-circle-outline"
+              size={26}
+              onPress={() => navigation.navigate("Profile")}
+            />
+            <MaterialCommunityIcons name="bell-outline" size={26} />
+          </View>
+        ),
+      }}
+    />
+    <HomeStack.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        title: "Profile",
+        headerRight: () => (
+          <View style={styles.iconContainer}>
+            <MaterialCommunityIcons
+              name="account-edit-outline"
+              size={30}
+              onPress={() => navigation.navigate("editProfile")}
+            />
+          </View>
+        ),
+      }}
+    />
+    <HomeStack.Screen
+      name="editProfile"
+      component={editProfileScreen}
+      options={{
+        title: "Edit Profile",
+      }}
+    />
+  </HomeStack.Navigator>
 );
-
 
 const DoctorsStackScreen = () => (
   <DoctorsStack.Navigator>
-        <DoctorsStack.Screen name="Doctors" component={DoctorsScreen} options={{headerShown:false,
-          title:'Doctors'
-        }} />
+    <DoctorsStack.Screen
+      name="Doctors"
+      component={DoctorsScreen}
+      options={{ headerShown: false, title: "Doctors" }}
+    />
   </DoctorsStack.Navigator>
 );
 
 const MoreStackScreen = () => (
   <MoreStack.Navigator>
-        <MoreStack.Screen name="More" component={MoreScreen} options={{headerShown:false,
-          title:'More'
-        }} />
+    <MoreStack.Screen
+      name="More"
+      component={MoreScreen}
+      options={{ headerShown: false, title: "More" }}
+    />
   </MoreStack.Navigator>
 );
 
 const ProfileStackScreen = () => (
   <ProfileStack.Navigator>
-    <ProfileStack.Screen name="Profle" component={ProfileScreen} options={{headerShown:false,
-      title:'Profile'
-    }}/>
+    <ProfileStack.Screen
+      name="Profle"
+      component={ProfileScreen}
+      options={{ headerShown: false, title: "Profile" }}
+    />
   </ProfileStack.Navigator>
-)
+);
 
 export default Tabs;
 
@@ -137,6 +158,5 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginLeft: 15,
     width: 70,
-  }
- 
+  },
 });
