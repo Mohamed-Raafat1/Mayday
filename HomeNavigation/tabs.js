ee3a8425804aa895f655165f4a4789682b33bb1e
fonze816 <muhammedraafat10@gmail.com>
2021-07-13 15:04:19 +0200

    Replaced reactnative elements with native base elements

--- HomeNavigation/tabs.js
+++ HomeNavigation/tabs.js
@@ -1,15 +1,12 @@
-
 //BUTTONS NATIVE BASE
-
 
 import React from "react";
 //React Native
-import { StyleSheet, Text, Image } from "react-native";
+import { StyleSheet, Image } from "react-native";
 
-
-import { createStackNavigator } from '@react-navigation/stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 // Screens
 import HomeScreen from "../Screens/HomeScreen";
@@ -18,42 +15,44 @@ import ProfileScreen from "../Screens/ProfileScreen";
 import editProfileScreen from "../Screens/editProfileScreen";
 import FirstAidSection from "../Screens/FirstAidSection";
 import ViewNearestHospital from "../Screens/ViewNearestHospital";
-import { Button ,Container } from "native-base";
-
+import { Button, Icon, Text, Container, Content, Right } from "native-base";
 
 const HomeStack = createStackNavigator();
 const FirstAidStack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
-
 function Tabs() {
-    return (
-      <Tab.Navigator
-          initialRouteName="Home"
-          activeColor="#ff1262"
-          barStyle={{ backgroundColor: 'white' }}
-        >
-          <Tab.Screen
-            name="Home"
-            component={HomeStackScreen}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="First Aid"
-            component={FirstAidStackScreen}
-            options={{
-              tabBarLabel: 'First Aid',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="television-play" color={color} size={26} />
-              ),
-            }}
-          />
-          {/* <Tab.Screen
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#ff1262"
+      barStyle={{ backgroundColor: "white" }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="First Aid"
+        component={FirstAidStackScreen}
+        options={{
+          tabBarLabel: "First Aid",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="television-play"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      {/* <Tab.Screen
             name="Doctors"
             component={DoctorsStackScreen}
             options={{
@@ -63,7 +62,7 @@ function Tabs() {
               ),
             }}
           /> */}
-          {/* <Tab.Screen
+      {/* <Tab.Screen
             name="More"
             component={MoreStackScreen}
             options={{
@@ -73,37 +72,45 @@ function Tabs() {
               ),
             }}
           /> */}
-        </Tab.Navigator>       
-      );
+    </Tab.Navigator>
+  );
 }
 
 const HomeStackScreen = ({ navigation }) => (
-
   <HomeStack.Navigator>
     <HomeStack.Screen
       name="Home"
       component={HomeScreen}
       options={{
         headerLeft: () => (
-          <Container style={styles.moreIcon}>
-            <MaterialCommunityIcons
-              name="menu"
-              size={26}
-              onPress={() => navigation.openDrawer()}
-            />       
-          </Container>
+          <Content style={styles.iconStyle}>
+            <Button transparent>
+              <MaterialCommunityIcons
+                name="menu"
+                size={26}
+                onPress={() => navigation.openDrawer()}
+              />
+            </Button>
+            {/*      */}
+          </Content>
         ),
-        title: "Home",
+        title: "Appname",
         headerRight: () => (
-          <Container style={styles.iconContainer}>
-            <MaterialCommunityIcons
-              name="account-circle-outline"
-              size={26}
-              onPress={() => navigation.navigate("Profile")}
-            />
-            <Button > </Button>
-            <MaterialCommunityIcons name="bell-outline" size={26} />
-          </Container>
+          <Content style={styles.iconStyle}>
+            <Button transparent>
+              <MaterialCommunityIcons
+                name="bell-outline"
+                size={26}
+                style={styles.icon}
+              />
+              <MaterialCommunityIcons
+                name="account-circle-outline"
+                size={26}
+                onPress={() => navigation.navigate("Profile")}
+                style={styles.icon}
+              />
+            </Button>
+          </Content>
         ),
       }}
     />
@@ -113,24 +120,26 @@ const HomeStackScreen = ({ navigation }) => (
       options={{
         title: "Profile",
         headerRight: () => (
-          <Container style={styles.iconContainer}>
-            <MaterialCommunityIcons
-              name="account-edit-outline"
-              size={30}
-              onPress={() => navigation.navigate("editProfile")}
-            />
-          </Container>
+          <Content style={styles.iconStyle}>
+            <Button transparent>
+              <MaterialCommunityIcons
+                name="account-edit-outline"
+                size={30}
+                onPress={() => navigation.navigate("editProfile")}
+              />
+            </Button>
+          </Content>
         ),
       }}
     />
     <HomeStack.Screen
       name="editProfile"
+      style={styles.icon}
       component={editProfileScreen}
       options={{
         title: "Edit Profile",
       }}
     />
- 
   </HomeStack.Navigator>
 );
 
@@ -156,22 +165,28 @@ const HomeStackScreen = ({ navigation }) => (
 
 const FirstAidStackScreen = () => (
   <FirstAidStack.Navigator>
-        <FirstAidStack.Screen name="FirstAid" component={FirstAidSection} options={{
-            headerLeft:null,
-            title:'FirstAid',
-        }} />
+    <FirstAidStack.Screen
+      name="FirstAid"
+      component={FirstAidSection}
+      options={{
+        headerLeft: null,
+        title: "FirstAid",
+      }}
+    />
   </FirstAidStack.Navigator>
 );
 export default Tabs;
-
 const styles = StyleSheet.create({
-  iconContainer: {
-    flexDirection: "row-reverse",
-    justifyContent: "space-between",
-    marginLeft: 15,
-    width: 70,
+  iconStyle: {
+    alignContent: "center",
+    marginLeft: 10,
+    marginTop: 5,
   },
-  moreIcon : {
-    marginLeft:10 ,
+  icon: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  Item: {
+    marginBottom: 10,
   },
 });
